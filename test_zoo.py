01826cb956bf943f9a789c6dbe25547feb5f2643
Phruek07 <79270034+Phruek07@users.noreply.github.com>
2024-11-11 08:41:34 +0000

    6687014_Tapanut

--- test_zoo.py
+++ test_zoo.py
@@ -5,10 +5,23 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
-    def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+    def test_notbornticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-5), "Error")
        
     # Add your additional test cases here.
+
+    def test_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_ticket21_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+    
+    def test_Oldman_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+    
+    def test_Oldman2_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+
 
 if __name__ == '__main__':
     unittest.main()
